307b050243ebe9ae0c866ffd608bbda4c5ec9e6d
Raphael Avelar <raphael.s.avelar@gmail.com>
2023-10-06 02:24:07 -0300

    feat: add global input component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Router } from './router/router'
 export { default as Button } from './button/button'
+export { default as Input } from './input/input'
---
+++ src/components/input/input.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+const Input: React.FC<Props> = (props: Props) => {
+  return (
+    <input
+      className="appearance-none block w-full border-2 placeholder-zinc-200 border-gray-400 text-white bg-zinc-900 rounded py-2 px-4 leading-tight focus:outline-none focus:border-purple-600"
+      {...props}
+    />
+  )
+}
+
+export default Input
