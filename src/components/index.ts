560f882f53a5930dfd09fda9e19b912e6e83a972
Raphael Avelar <raphael.s.avelar@gmail.com>
2023-10-06 02:24:57 -0300

    feat: add global select component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Router } from './router/router'
 export { default as Button } from './button/button'
 export { default as Input } from './input/input'
+export { default as Select } from './select/select'
---
+++ src/components/select/select.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface Props extends React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement> {
+  options: Array<string>
+}
+
+const Select: React.FC<Props> = (props: Props) => {
+  const { options } = props
+
+  return (
+    <div className="inline-block relative w-64">
+      <select
+        className="block appearance-none w-full bg-zinc-900 text-white border-2 border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline focus:border-purple-600"
+        {...props}
+      >
+        {options.map((option, i) => (
+          <option key={i}>{option}</option>
+        ))}
+      </select>
+
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-white">
+        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+        </svg>
+      </div>
+    </div>
+  )
+}
+
+export default Select
