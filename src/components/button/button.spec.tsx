85a3f047bf401c3014feb43e4c2bb42233b3ab15
Raphael Avelar <raphael.s.avelar@gmail.com>
2023-10-08 20:34:13 -0300

    test: improve test component button

--- src/components/button/button.spec.tsx
+++ src/components/button/button.spec.tsx
@@ -1,31 +1,27 @@
-import Button from "./button"
-import { ButtonType } from '../constants/button-types'
+import { Button, ButtonType } from '.'
 import { render, screen } from '@testing-library/react'
-import { faker } from '@faker-js/faker'
+import Chance from 'chance'
+const chance = new Chance()
+
+const setup = () => {
+  const label = chance.word()
+  const callAction = jest.fn()
+
+  render(<Button variant={ButtonType.Success} label={label} onClick={callAction} />)
+  return { callAction }
+}
 
 describe('Button Component', () => {
   test('Should render correctly', () => {
-    render(<Button variant={ButtonType.Success} label={faker.lorem.word()} />)
-    const button = screen.getByRole('button-' + ButtonType.Success)
+    setup()
+    const button = screen.getByRole('button')
     expect(button).toBeInTheDocument()
   })
 
   test('Should call on click', () => {
-    const actionClicked = jest.fn()
-    render(<Button variant={ButtonType.Success} label={faker.lorem.word()} onClick={actionClicked} />)
-    const button = screen.getByRole('button-' + ButtonType.Success)
+    const { callAction } = setup()
+    const button = screen.getByRole('button')
     button.click()
-    expect(actionClicked).toHaveBeenCalledTimes(1)
-  })
-
-  test('Should display only one variant', () => {
-    render(<Button variant={ButtonType.Success} label={faker.lorem.word()} />)
-    const buttonSuccess = screen.queryByRole('button-' + ButtonType.Success)
-    expect(buttonSuccess).toBeInTheDocument()
-
-    const buttonAlert = screen.queryByRole('button-' + ButtonType.Alert)
-    expect(buttonAlert).not.toBeInTheDocument()
-    const buttonErro = screen.queryByRole('button-' + ButtonType.Erro)
-    expect(buttonErro).not.toBeInTheDocument()
+    expect(callAction).toHaveBeenCalledTimes(1)
   })
 })
