2f9bee261111890c42c5b8df5e9b82af20ac1145
Raphael Avelar <raphael.s.avelar@gmail.com>
2023-10-09 01:39:15 -0300

    refactor: improve input/router components

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
+interface Props extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {}
 
-type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
-
-const Input: React.FC<Props> = (props: Props) => {
+const Input = (props: Props) => {
   return (
     <input
       className="appearance-none block w-full border-2 placeholder-zinc-200 border-gray-400 text-white bg-zinc-900 rounded py-2 px-4 leading-tight focus:outline-none focus:border-purple-600"
