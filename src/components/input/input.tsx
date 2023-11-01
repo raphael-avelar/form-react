42ba0e9c227d4917ddcb6177c107897519112ec0
Raphael Avelar <raphael.s.avelar@gmail.com>
2023-11-01 20:05:03 -0300

    refactor: improve input component

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,10 +1,15 @@
-interface Props extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {}
+interface Props {
+  name: string
+  value: string
+  placeholder: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
 
-const Input = (props: Props) => {
+const Input = ({name, ...props}: Props) => {
   return (
     <input
       className="appearance-none block w-full border-2 placeholder-zinc-200 border-gray-400 text-white bg-zinc-900 rounded py-2 px-4 leading-tight focus:outline-none focus:border-purple-600"
-      role={props.name}
+      role={name}
       {...props}
     />
   )
