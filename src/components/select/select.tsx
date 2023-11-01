b73c6e7af1d19726c78e0276148921184f708f3f
Raphael Avelar <raphael.s.avelar@gmail.com>
2023-11-01 20:05:24 -0300

    refactor: improve select component

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -1,19 +1,22 @@
-interface Props extends React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement> {
+interface Props {
+  name: string
+  value: string
   options: Array<string>
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void
 }
 
-const Select = ({options, ...props}: Props) => {
+const Select = ({name, options, ...props}: Props) => {
   return (
     <div className="inline-block relative w-64">
       <select
         className="block appearance-none w-full bg-zinc-900 text-white border-2 border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline focus:border-purple-600"
-        role={props.name}
+        role={name}
         {...props}
       >
         {options.map((option, key) => (
           <option
-            key={`option-${key}`}
-            role={`option-${props.name}`}
+            key={`${name}-${key}`}
+            role={`option-${name}`}
           >
             {option}
           </option>
